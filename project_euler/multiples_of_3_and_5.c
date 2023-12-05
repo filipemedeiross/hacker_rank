2c3b42bcb4be8dbcabf6f8b6454814f78f2d3877
filipemedeiross <filipemedeiros2013@gmail.com>
2023-12-05 13:56:58 -0300

    Refactoring the first challenge of the Project Euler

--- project_euler/multiples_of_3_and_5.c
+++ project_euler/multiples_of_3_and_5.c
@@ -3,23 +3,23 @@
 
 int main () {
     int t, n;
-    long int sum;
+    long int p, q, r;
+    long int s3, s5, s15, sum;
 
     scanf("%d", &t);
     
-    sum = 0;
     for (int a0 = 0; a0 < t; a0++) {
         scanf("%d", &n);
         
         n--;
 
-        long int p = n / 3;
-        long int q = n / 5;
-        long int r = n / 15;
+        p = n / 3;
+        q = n / 5;
+        r = n / 15;
 
-        long int s3 = (p * (2 * 3 + (p - 1) * 3)) / 2;
-        long int s5 = (q * (2 * 5 + (q - 1) * 5)) / 2;
-        long int s15 = (r * (2 * 15 + (r - 1) * 15)) / 2;
+        s3  = (3 * p * (p + 1)) / 2;
+        s5  = (5 * q * (q + 1)) / 2;
+        s15 = (15 * r * (r + 1)) / 2;
 
         sum = s3 + s5 - s15;
         printf("%ld\n", sum);
